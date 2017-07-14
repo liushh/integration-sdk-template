60b14aeea7111e809321bdec17244ffdf60583cf
Liusha <liusha@wizeline.com>
2017-07-14 10:24:04 -0500

    added minimal structure for integration sdk template

---
+++ sdk/__init__.py
@@ -0,0 +1,2 @@
+def get_name():
+    print('this function should return the name of this integration')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='liusha-integration-sdk-tempalte',
+      version='0.1',
+      description='The best integration sdk template',
+      url='https://github.com/liushh/integration-sdk-template.git',
+      author='Liusha',
+      author_email='hlswh1021@gmail.com',
+      license='MIT',
+      packages=['liusha-integration-sdk-template'],
+      zip_safe=False)
